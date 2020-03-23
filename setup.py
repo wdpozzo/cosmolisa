2a5245086a67b06effbd0a48032112bafa8240ab
Stefano Rinaldi <s.rinaldi9@studenti.unipi.it>
2020-03-23 09:56:22 +0100

    update

--- setup.py
+++ setup.py
@@ -7,28 +7,33 @@ ext_modules=[
              Extension("cosmology",
                        sources=["cosmology.pyx"],
                        libraries=["m","lal"], # Unix-like specific
-                       library_dirs = ["/Users/wdp/opt/master/lib"],
-                       include_dirs=[numpy.get_include(),"/Users/wdp/opt/master/include"]
+                       # library_dirs = ["/Users/wdp/opt/master/lib"],
+                       library_dirs = ["/Users/Stefano/anaconda3/lib"],
+                       # include_dirs=[numpy.get_include(),"/Users/wdp/opt/master/include"] #(LAL?)
+                       include_dirs=[numpy.get_include(),"/Users/Stefano/anaconda3/include"]
                        )
              ]
 
 setup(
       name = "cosmology",
       ext_modules = cythonize(ext_modules),
-      include_dirs=[numpy.get_include(),"/Users/wdp/opt/master/include"]
+      # include_dirs=[numpy.get_include(),"/Users/wdp/opt/master/include"]
+      include_dirs=[numpy.get_include(), "/Users/Stefano/anaconda3/include"]
       )
 ext_modules=[
              Extension("likelihood",
                        sources=["likelihood.pyx"],
                        libraries=["m","lal"], # Unix-like specific
-                       library_dirs = ["/Users/wdp/opt/master/lib"],
-                       include_dirs=[numpy.get_include(),"/Users/wdp/opt/master/include"]
+                       # library_dirs = ["/Users/wdp/opt/master/lib"],
+                       library_dirs = ["/Users/Stefano/anaconda3/lib"],
+                       # include_dirs=[numpy.get_include(),"/Users/wdp/opt/master/include"] #(LAL?)
+                       include_dirs=[numpy.get_include(), "/Users/Stefano/anaconda3/include"]
                        )
              ]
 
 setup(
       name = "likelihood",
       ext_modules = cythonize(ext_modules),
-      include_dirs=[numpy.get_include(),"/Users/wdp/opt/master/include"]
+      # include_dirs=[numpy.get_include(),"/Users/wdp/opt/master/include"]
+      include_dirs=[numpy.get_include(), "/Users/Stefano/anaconda3/include"]
       )
-
